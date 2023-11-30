b49817b4ed6215510653304eea61de89b5e87396
Vitali Bestolkau <bestolkovv@gmail.com>
2023-12-01 00:14:44 +0100

    feat: added more statement management

--- src/models/statementModel.ts
+++ src/models/statementModel.ts
@@ -3,19 +3,35 @@ import {Schema, model, Types} from "mongoose"
 export interface IStatement {
     actor: Types.ObjectId,
     action: string,
-    object: string,
-    createdAt?: Date,
-    updatedAt?: Date
+    object: Website,
+    openedAt: Date,
+    closedAt: Date
 }
 
-const statementSchema = new Schema<IStatement>(
-    {
+export interface IReqStatement {
+    actor: Types.ObjectId,
+    action: string,
+    object: Website,
+    openedAt: string,
+    closedAt: string
+}
+
+export interface Website {
+    hostname: string,
+    favIconUrl?: string,
+    title?: string
+}
+
+const statementSchema = new Schema<IStatement>({
     actor: {type: Schema.Types.ObjectId, ref: "Users"},
     action: {type: String, required: true},
-    object: String
+    object: {
+        hostname: {type: String, required: true},
+        favIconUrl: String,
+        title: String
     },
-    {
-        timestamps: true
-    })
+    openedAt: Date,
+    closedAt: Date
+})
 
 export const StatementModel = model<IStatement>("Statements", statementSchema, "statements")
